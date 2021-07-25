4a54b8f6c3d984572b509d4cf8981c0d144cdf95
tsalapovaMaria <mazhats@yandex.ru>
2021-07-25 15:39:53 +0300

    toggle class on current currency on dropdown label

--- scripts/close-dropdown.js
+++ scripts/close-dropdown.js
@@ -1,21 +1,36 @@
 const toggler = document.querySelector('#dropdown');
 
-
 const dropdownContainer = document.querySelector('.dropdown-container');
 const dropdownList = document.querySelector('.dropdown');
 const dropdownItems = document.querySelector('.items');
 
+const dropdownLabels = dropdownItems.querySelectorAll('LABEL');
 
-if(toggler.checked){
-    dropdownContainer.addEventListener('click', (e) => {
-        const target = e.target;
+
+let wasChecked = false;
+
+
+dropdownList.addEventListener('click', () => {
+    const toggleFunc = () => {    
+        const currentCurrency = document.querySelector('#text > div').textContent;
     
-        if(this.className !== '.dropdown'){
-            console.log('here');
-            return;
-        }
+        const label = Array.from(dropdownLabels).find(label => label.textContent === currentCurrency);
         
-        console.log('here');
+        if(isChecked && label.className === ''){
+            label.className = 'active-currency';
+        }
+        if(!isChecked && label.className !== ''){
+            label.className = '';
+        }
+    }
+
+    const isChecked = toggler.checked;
+
+    toggleFunc();
+
+    if (wasChecked && isChecked) {
         toggler.checked = false;
-    });
-}
+    }
+    wasChecked = isChecked;
+
+});
