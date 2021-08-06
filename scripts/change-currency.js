130742c909a8c889bdef6bb3bc6eb7f6634e2c40
tsalapovaMaria <mazhats@yandex.ru>
2021-08-06 13:29:42 +0300

    avoid functions recreation

--- scripts/change-currency.js
+++ scripts/change-currency.js
@@ -2,64 +2,67 @@ const inputElements = document.querySelectorAll('.items__wrapper > input[type=ra
 const currentCurrencyElement = document.querySelector('.current-container__value');
 const tableSpanElements = document.querySelectorAll('.price-container__currency');
 
+
+const changeCurrency = (inputValue) => {
+    switch (inputValue) {
+        case 'usd':
+            currency = '$';
+            break;
+        case 'eur':
+            currency = '€';
+            break;
+        case 'rub':
+            currency = '₽';
+            break;
+    }
+};
+
+const changePlaceholderCurrency = () => {
+    const changePlaceholder = (value, input) => {
+        input.placeholder = `${value} ${currency}`;
+    }
+    const priceInputs = document.querySelectorAll('.price-container__input');
+
+    Array.from(priceInputs).forEach(
+        input => {
+            changePlaceholder(0, input);
+        }
+    );
+
+    const priceInputAverage = document.querySelectorAll('.current-price__input');
+    Array.from(priceInputAverage).forEach(
+        input => {
+            changePlaceholder(200, input);
+        }
+    );
+
+    const desiredPriceInput = document.querySelector('.desired-price__input');
+    changePlaceholder(205, desiredPriceInput);
+};
+
+const changeCurrencyElements = () => {
+    const currencyElements = document.querySelectorAll('[data-currency]');
+
+    Array.from(currencyElements).forEach(
+        element => {
+            element.dataset.currency = currency;
+        }
+    );
+};
+
+const changePriceInputs = () => {
+    Array.from(tableSpanElements).forEach(
+        span => {
+            span.dataset.currency = currency;
+        }
+    );
+};
+
 Array.from(inputElements).forEach(input =>
     input.addEventListener('change', () => {
         if (!input.checked) {
             return;
         }
-
-        const changeCurrency = (inputValue) => {
-            switch (inputValue) {
-                case 'usd':
-                    currency = '$';
-                    break;
-                case 'eur':
-                    currency = '€';
-                    break;
-                case 'rub':
-                    currency = '₽';
-                    break;
-            }
-        };
-
-        const changePlaceholderCurrency = () => {
-            const priceInputs = document.querySelectorAll('.price-container__input');
-
-            Array.from(priceInputs).forEach(
-                input => {
-                    input.placeholder = '0 ' + currency;
-                }
-            );
-
-            const priceInputAverage = document.querySelectorAll('.current-price__input');
-            Array.from(priceInputAverage).forEach(
-                input => {
-                    input.placeholder = '220 ' + currency;
-                }
-            );
-
-            const desiredPriceInput = document.querySelector('.desired-price__input');
-            desiredPriceInput.placeholder = '205 ' + currency;
-        };
-
-        const changeCurrencyElements = () => {
-            const currencyElements = document.querySelectorAll('[data-currency]');
-        
-            Array.from(currencyElements).forEach(
-                element => {
-                    element.dataset.currency = currency;
-                }
-            );
-        };
-
-        const changePriceInputs = () => {
-            Array.from(tableSpanElements).forEach(
-                span => {
-                    span.dataset.currency = currency;
-                }
-            );
-        };
-
         changeCurrency(input.value);
 
         currentCurrencyElement.textContent = input.value.toUpperCase();
